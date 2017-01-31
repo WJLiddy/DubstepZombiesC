769e0d97afa9e4c08374081bad1b4a8c8e8458ed
WJLIDDY <wjl36@case.edu>
2017-01-31 18:25:24 -0500

    CMake fix for Ingame

--- src/gamestate/ingame.cpp
+++ src/gamestate/ingame.cpp
@@ -2,3 +2,14 @@
 #include "ingame.h"
 
 
+    GameState* InGame::update_state()   
+    {
+        return NULL;
+    }
+
+    
+    void InGame::draw_state(DrawUtils& drawUtils)  
+    {
+        al_clear_to_color(al_map_rgb(50,10,70));
+        drawUtils.drawCenteredString(255,0,0,drawUtils.GAME_W/2,32,"U R PLAYIN GAEM");
+    }
--- src/gamestate/ingame.h
+++ src/gamestate/ingame.h
@@ -10,17 +10,9 @@ class InGame : public GameState
 public:	
 	InGame(Inputs* inputs) : GameState(inputs) {}
   
-    GameState* update_state()   
-    {
-        return NULL;
-    }
+    GameState* update_state();
 
-    
-    void draw_state(DrawUtils& drawUtils)  
-    {
-        al_clear_to_color(al_map_rgb(50,10,70));
-        drawUtils.drawCenteredString(255,0,0,drawUtils.GAME_W/2,32,"U R PLAYIN GAEM");
-    }
+    void draw_state(DrawUtils& drawUtils);
 
 };
 #endif
